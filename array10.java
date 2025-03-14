2c23c26fc77abf4b8309892c088a604283e85ac4
Revti Ranjan Jha <revtiranjanjha1209@gmail.com>
2025-03-14 21:58:25 +0530

    lalla ka commit

--- array10.java
+++ array10.java
@@ -1,13 +1,5 @@
 public class array10 {
     public  static void main(String[] args){
         int[]  numbers = {1, 2, 3, 4};
-
-        for(int i = 0; i < numbers.length; i++){
-            numbers[i] = numbers[i]*5;
-        }
-
-        System.out.println("new array ");
-        System.out.print(numbers[i] + " ");
-
     }
 }
